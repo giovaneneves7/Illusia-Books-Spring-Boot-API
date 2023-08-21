080917d6ffc3abfd4425996f93f3dc37abba8c03
NekoYasha <autornekoyasha@gmail.com>
2023-08-21 11:50:24 -0300

    Add comments

--- src/main/java/com/illusia/api/IllusiaBooksApiApplication.java
+++ src/main/java/com/illusia/api/IllusiaBooksApiApplication.java
@@ -3,6 +3,12 @@ package com.illusia.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author Giovane Neves
+ * Since v1.0.0
+ * Created 2023-08-21 11:43 AM
+ */
 @SpringBootApplication
 public class IllusiaBooksApiApplication {
 
