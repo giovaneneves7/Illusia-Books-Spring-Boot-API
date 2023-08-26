329942043475f07f38866da73d9338a11164ed61
NekoYasha <autornekoyasha@gmail.com>
2023-08-26 13:09:36 -0300

    Create 'banner DTO' to the 'create banner' responses

--- src/main/java/com/illusia/api/entity/banner/model/dto/response/CreateBannerResponseDto.java
+++ src/main/java/com/illusia/api/entity/banner/model/dto/response/CreateBannerResponseDto.java
@@ -1,2 +1,15 @@
-package com.illusia.api.entity.banner.model.dto.response;public record CreateBannerResponse() {
+package com.illusia.api.entity.banner.model.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.UUID;
+
+public record CreateBannerResponseDto(
+
+        @JsonProperty(value = "id")
+        UUID id,
+
+        @JsonProperty(value = "name")
+        String name
+) {
 }
