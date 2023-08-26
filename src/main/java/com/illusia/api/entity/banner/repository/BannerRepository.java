23ff87690db8588e88958c77b8dc9e37a07e9cb3
NekoYasha <autornekoyasha@gmail.com>
2023-08-26 13:07:59 -0300

    Create banner repository

--- src/main/java/com/illusia/api/entity/banner/repository/BannerRepository.java
+++ src/main/java/com/illusia/api/entity/banner/repository/BannerRepository.java
@@ -1,2 +1,12 @@
-package com.illusia.api.entity.banner.repository;public interface BannerRepository {
+package com.illusia.api.entity.banner.repository;
+
+import com.illusia.api.entity.banner.model.Banner;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BannerRepository extends JpaRepository<Banner, UUID> {
 }
