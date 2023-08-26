c81dd8037d9c8a017a56a0b2c97f595a4353d515
NekoYasha <autornekoyasha@gmail.com>
2023-08-26 13:10:30 -0300

    Create banner service

--- src/main/java/com/illusia/api/entity/banner/service/BannerService.java
+++ src/main/java/com/illusia/api/entity/banner/service/BannerService.java
@@ -1,2 +1,44 @@
-package com.illusia.api.entity.banner.service;public class BannerService {
+package com.illusia.api.entity.banner.service;
+
+import com.illusia.api.entity.banner.model.Banner;
+import com.illusia.api.entity.banner.model.dto.request.CreateBannerRequestDto;
+
+import com.illusia.api.entity.banner.model.dto.response.CreateBannerResponseDto;
+import com.illusia.api.entity.banner.repository.BannerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BannerService implements IBannerService{
+
+
+    @Autowired
+    BannerRepository bannerRepository;
+
+
+    /**
+     * Saves a new banner in the database.
+     *
+     * @param bannerDto The object with the banner data to be saved.
+     * @return A DTO object with the ID and name of the banner saved.
+     */
+    @Override
+    public CreateBannerResponseDto createBanner(final CreateBannerRequestDto bannerDto) {
+
+        List<Banner> banners = bannerRepository.findAll();
+
+        Banner banner = new Banner();
+        bannerDto.convert(banner);
+
+        if(!banners.isEmpty()){
+
+            // Logic for banner name validation...
+        }
+
+        bannerRepository.save(banner);
+
+        return new CreateBannerResponseDto(banner.getId(),banner.getName());
+    }
 }
--- src/main/java/com/illusia/api/entity/banner/service/IBannerService.java
+++ src/main/java/com/illusia/api/entity/banner/service/IBannerService.java
@@ -1,2 +1,15 @@
-package com.illusia.api.entity.banner.service;public class IBannerService {
+package com.illusia.api.entity.banner.service;
+
+import com.illusia.api.entity.banner.model.dto.request.CreateBannerRequestDto;
+import com.illusia.api.entity.banner.model.dto.response.CreateBannerResponseDto;
+
+public interface IBannerService {
+
+    /**
+     * Saves a new banner in the database.
+     *
+     * @param bannerDto The object with the banner data to be saved.
+     * @return A DTO object with the ID and name of the banner saved.
+     */
+    CreateBannerResponseDto createBanner(final CreateBannerRequestDto bannerDto);
 }
