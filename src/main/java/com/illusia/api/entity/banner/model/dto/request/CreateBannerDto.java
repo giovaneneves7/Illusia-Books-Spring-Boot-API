24f78a0c1ea0f74b5c40b605760eb4b54eacb0a2
NekoYasha <autornekoyasha@gmail.com>
2023-08-26 11:27:50 -0300

    Add validation dependency

---
+++ src/main/java/com/illusia/api/entity/banner/model/dto/request/CreateBannerDto.java
@@ -0,0 +1,2 @@
+package com.illusia.api.entity.banner.model.dto.request;public record CreateBannerDto() {
+}
---
+++ src/main/java/com/illusia/api/entity/banner/repository/BannerRepository.java
@@ -0,0 +1,2 @@
+package com.illusia.api.entity.banner.repository;public interface BannerRepository {
+}
