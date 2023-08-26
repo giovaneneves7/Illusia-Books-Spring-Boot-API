e030e5b8071877b552d3b9f2937c12d530d913f3
NekoYasha <autornekoyasha@gmail.com>
2023-08-26 13:10:04 -0300

    Create API Facade

--- src/main/java/com/illusia/api/service/Facade.java
+++ src/main/java/com/illusia/api/service/Facade.java
@@ -1,2 +1,30 @@
-package com.illusia.api.service;public class Facade {
+package com.illusia.api.service;
+
+import com.illusia.api.entity.banner.model.dto.request.CreateBannerRequestDto;
+import com.illusia.api.entity.banner.model.dto.response.CreateBannerResponseDto;
+import com.illusia.api.entity.banner.service.IBannerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Facade implements IFacade{
+
+    @Autowired
+    IBannerService bannerService;
+
+    // ======================{ - BANNERS - }====================== //
+
+    /**
+     * Saves a new banner in the database.
+     *
+     * @param bannerDto The object with the banner data to be saved.
+     * @return A DTO object with the ID and name of the banner saved.
+     */
+    @Override
+    public CreateBannerResponseDto createBanner(CreateBannerRequestDto bannerDto) {
+
+        return this.bannerService.createBanner(bannerDto);
+
+    }
 }
--- src/main/java/com/illusia/api/service/IFacade.java
+++ src/main/java/com/illusia/api/service/IFacade.java
@@ -1,2 +1,17 @@
-package com.illusia.api.service;public interface IFacade {
+package com.illusia.api.service;
+
+import com.illusia.api.entity.banner.model.dto.request.CreateBannerRequestDto;
+import com.illusia.api.entity.banner.model.dto.response.CreateBannerResponseDto;
+
+public interface IFacade {
+
+    // ======================{ - BANNERS - }====================== //
+
+    /**
+     * Saves a new banner in the database.
+     *
+     * @param bannerDto The object with the banner data to be saved.
+     * @return A DTO object with the ID and name of the banner saved.
+     */
+    CreateBannerResponseDto createBanner(CreateBannerRequestDto bannerDto);
 }
